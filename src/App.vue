2b1adc66fc7ad22c28c7e18497c1b009bff6a4b6
Philo van Kemenade <philovankemenade@gmail.com>
2020-03-19 16:19:30 +0100

    plot full episode

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,104 @@
 <template>
   <div id="app">
     <h1>Frame Viewer</h1>
+    <!-- <FrameViewer :framePaths="framePathsEpisode"/> -->
+    <h2>2005_150509@program</h2>
+    <FrameViewer
+      :frameDir="frameDir1"
+      :frameMaxIndex="frameMaxIndex1"
+      :framePrefix="framePrefix1"
+    />
 
-    <h2>2005</h2>
+    <!-- <h2>2005</h2>
     <FrameViewer :framePaths="framePaths1"/>
     <h2>2011</h2>
     <FrameViewer :framePaths="framePaths2"/>
     <h2>2018</h2>
-    <FrameViewer :framePaths="framePaths3"/>
+    <FrameViewer :framePaths="framePaths3"/> -->
   </div>
 </template>
 
 <script>
-import FrameViewer from './components/FrameViewer.vue'
+import FrameViewer from "./components/FrameViewer.vue";
 
 export default {
-  name: 'App',
-  data: function () {
+  name: "App",
+  data: function() {
     return {
+      frameDir1: "2005_150509@program-sequential",
+      framePrefix1: "2005_150509@program-",
+      frameMaxIndex1: 2595,
       framePaths1: [
-        '2005_150509@shot/keyframe_13.jpg',
-        '2005_150509@shot/keyframe_32.jpg',
-        '2005_150509@shot/keyframe_46.jpg',
-        '2005_150509@shot/keyframe_50.jpg',
-        '2005_150509@shot/keyframe_88.jpg',
-        '2005_150509@shot/keyframe_97.jpg',
-        '2005_150509@shot/keyframe_122.jpg',
-        '2005_150509@shot/keyframe_129.jpg',
-        '2005_150509@shot/keyframe_155.jpg',
-        '2005_150509@shot/keyframe_163.jpg',
-        '2005_150509@shot/keyframe_177.jpg',
-        '2005_150509@shot/keyframe_193.jpg',
-        '2005_150509@shot/keyframe_215.jpg',
-        '2005_150509@shot/keyframe_235.jpg',
-        '2005_150509@shot/keyframe_244.jpg',
-        '2005_150509@shot/keyframe_263.jpg',
-        '2005_150509@shot/keyframe_280.jpg',
-        '2005_150509@shot/keyframe_281.jpg',
-        '2005_150509@shot/keyframe_324.jpg',
-        '2005_150509@shot/keyframe_350.jpg',
-        '2005_150509@shot/keyframe_353.jpg',
-        '2005_150509@shot/keyframe_375.jpg',
-        '2005_150509@shot/keyframe_385.jpg',
-        '2005_150509@shot/keyframe_404.jpg',
-        '2005_150509@shot/keyframe_414.jpg',
-        '2005_150509@shot/keyframe_430.jpg',
-        '2005_150509@shot/keyframe_442.jpg',
-        '2005_150509@shot/keyframe_460.jpg',
-        '2005_150509@shot/keyframe_475.jpg',
-        '2005_150509@shot/keyframe_505.jpg',
-        '2005_150509@shot/keyframe_525.jpg',
-        '2005_150509@shot/keyframe_529.jpg',
-        '2005_150509@shot/keyframe_574.jpg',
-        '2005_150509@shot/keyframe_588.jpg',
-        '2005_150509@shot/keyframe_593.jpg',
-        '2005_150509@shot/keyframe_609.jpg',
-        '2005_150509@shot/keyframe_624.jpg',
-        '2005_150509@shot/keyframe_640.jpg',
-        '2005_150509@shot/keyframe_641.jpg',
-        '2005_150509@shot/keyframe_663.jpg',
-        '2005_150509@shot/keyframe_679.jpg',
-        '2005_150509@shot/keyframe_689.jpg',
-        '2005_150509@shot/keyframe_705.jpg',
-        '2005_150509@shot/keyframe_737.jpg',
+        "2005_150509@shot/keyframe_13.jpg",
+        "2005_150509@shot/keyframe_32.jpg",
+        "2005_150509@shot/keyframe_46.jpg",
+        "2005_150509@shot/keyframe_50.jpg",
+        "2005_150509@shot/keyframe_88.jpg",
+        "2005_150509@shot/keyframe_97.jpg",
+        "2005_150509@shot/keyframe_122.jpg",
+        "2005_150509@shot/keyframe_129.jpg",
+        "2005_150509@shot/keyframe_155.jpg",
+        "2005_150509@shot/keyframe_163.jpg",
+        "2005_150509@shot/keyframe_177.jpg",
+        "2005_150509@shot/keyframe_193.jpg",
+        "2005_150509@shot/keyframe_215.jpg",
+        "2005_150509@shot/keyframe_235.jpg",
+        "2005_150509@shot/keyframe_244.jpg",
+        "2005_150509@shot/keyframe_263.jpg",
+        "2005_150509@shot/keyframe_280.jpg",
+        "2005_150509@shot/keyframe_281.jpg",
+        "2005_150509@shot/keyframe_324.jpg",
+        "2005_150509@shot/keyframe_350.jpg",
+        "2005_150509@shot/keyframe_353.jpg",
+        "2005_150509@shot/keyframe_375.jpg",
+        "2005_150509@shot/keyframe_385.jpg",
+        "2005_150509@shot/keyframe_404.jpg",
+        "2005_150509@shot/keyframe_414.jpg",
+        "2005_150509@shot/keyframe_430.jpg",
+        "2005_150509@shot/keyframe_442.jpg",
+        "2005_150509@shot/keyframe_460.jpg",
+        "2005_150509@shot/keyframe_475.jpg",
+        "2005_150509@shot/keyframe_505.jpg",
+        "2005_150509@shot/keyframe_525.jpg",
+        "2005_150509@shot/keyframe_529.jpg",
+        "2005_150509@shot/keyframe_574.jpg",
+        "2005_150509@shot/keyframe_588.jpg",
+        "2005_150509@shot/keyframe_593.jpg",
+        "2005_150509@shot/keyframe_609.jpg",
+        "2005_150509@shot/keyframe_624.jpg",
+        "2005_150509@shot/keyframe_640.jpg",
+        "2005_150509@shot/keyframe_641.jpg",
+        "2005_150509@shot/keyframe_663.jpg",
+        "2005_150509@shot/keyframe_679.jpg",
+        "2005_150509@shot/keyframe_689.jpg",
+        "2005_150509@shot/keyframe_705.jpg",
+        "2005_150509@shot/keyframe_737.jpg"
       ],
       framePaths2: [
-        '2011_3853265@shot/keyframe_4497.jpg',
-        '2011_3853265@shot/keyframe_4479.jpg',
-        '2011_3853265@shot/keyframe_4482.jpg',
-        '2011_3853265@shot/keyframe_4513.jpg',
-        '2011_3853265@shot/keyframe_4541.jpg',
-        '2011_3853265@shot/keyframe_4564.jpg',
-        '2011_3853265@shot/keyframe_4577.jpg',
-        '2011_3853265@shot/keyframe_4593.jpg',
-        '2011_3853265@shot/keyframe_4610.jpg',
-        '2011_3853265@shot/keyframe_4657.jpg',
-        '2011_3853265@shot/keyframe_4701.jpg',
-        '2011_3853265@shot/keyframe_4743.jpg',
+        "2011_3853265@shot/keyframe_4497.jpg",
+        "2011_3853265@shot/keyframe_4479.jpg",
+        "2011_3853265@shot/keyframe_4482.jpg",
+        "2011_3853265@shot/keyframe_4513.jpg",
+        "2011_3853265@shot/keyframe_4541.jpg",
+        "2011_3853265@shot/keyframe_4564.jpg",
+        "2011_3853265@shot/keyframe_4577.jpg",
+        "2011_3853265@shot/keyframe_4593.jpg",
+        "2011_3853265@shot/keyframe_4610.jpg",
+        "2011_3853265@shot/keyframe_4657.jpg",
+        "2011_3853265@shot/keyframe_4701.jpg",
+        "2011_3853265@shot/keyframe_4743.jpg"
       ],
       framePaths3: [
-        '2018_5353716@shot/keyframe_49.jpg',
-        '2018_5353716@shot/keyframe_128.jpg',
-        '2018_5353716@shot/keyframe_171.jpg',
-      ],
-
-    }
+        "2018_5353716@shot/keyframe_49.jpg",
+        "2018_5353716@shot/keyframe_128.jpg",
+        "2018_5353716@shot/keyframe_171.jpg"
+      ]
+    };
   },
   components: {
     FrameViewer
   }
-}
+};
 </script>
 
 <style>
--- src/components/FrameViewer.vue
+++ src/components/FrameViewer.vue
@@ -1,32 +1,48 @@
 <template>
   <div>
     <div>
-      <img 
-        v-for="(framePath, index) in framePaths" :key="framePath"
-        :alt="'frame'+index" :src="require(`@/assets/${framePath}`)"
+      <img
+        v-for="(framePath, index) in framePathsComputed"
+        :key="framePath"
+        :alt="'frame' + index"
+        :src="require(`@/assets/${framePath}`)"
         :style="{ width: slitWidth + 'px' }"
-        class="h4 object-fit" 
-      >
+        class="h4 object-fit"
+      />
     </div>
-    <input type="range" min="2" max="170" step="1" v-model="slitWidth"> 
-    <input type="number" v-model="slitWidth"/>
+    <input type="range" min="2" max="170" step="1" v-model="slitWidth" />
+    <input type="number" v-model="slitWidth" />
   </div>
 </template>
 
 <script>
-import '../../node_modules/tachyons/css/tachyons.min.css';
+import "../../node_modules/tachyons/css/tachyons.min.css";
 
 export default {
-  name: 'FrameViewer',
-  data: function () {
+  name: "FrameViewer",
+  data: function() {
     return {
-      slitWidth: 50
-    }
+      slitWidth: 1
+    };
   },
   props: {
-    framePaths: {type: Array}
+    framePaths: { type: Array },
+    frameDir: { type: String },
+    framePrefix: { type: String },
+    frameMaxIndex: { type: Number }
+  },
+  computed: {
+    framePathsComputed() {
+      if (this.framePaths) {
+        return this.framePaths;
+      } else {
+        return [...Array(this.frameMaxIndex).keys()].map(
+          i => `${this.frameDir}/${this.framePrefix}${i}.jpg`
+        );
+      }
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
